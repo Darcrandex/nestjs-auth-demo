3956f945672180dbeacd142661d7128688bca4c6
darcrand <darcrandex@gmail.com>
2022-11-29 23:48:29 +0800

    ✨ feat: jwt 全局守卫

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { JwtGuard } from './auth/jwt.guard'
 import { UserModule } from './user/user.module'
 
 @Module({
   imports: [AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+
+    // 使用全局守卫
+    // 相对于每个 controller 都加上
+    // @UseGuards(JwtGuard)
+    { provide: APP_GUARD, useClass: JwtGuard },
+  ],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,34 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common'
-import { JwtGuard } from 'src/auth/jwt.guard'
+import { BadRequestException, Controller, Get, Param, Request } from '@nestjs/common'
+import { PublicRoute } from 'src/auth/jwt.guard'
 import { User, UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // @UseGuards(JwtGuard)
+  @Get('info')
+  async userInfo(@Request() req: { user: User }) {
+    console.log('3. 接口接收从 JwtStrategy 返回的内容', req.user)
+    const user = await this.userService.findById(req.user.id)
+    return user
+  }
+
   @Get()
-  getUsers() {
+  async getUsers() {
     return this.userService.findAll()
   }
 
-  @UseGuards(JwtGuard)
-  @Get('info')
-  getUserById(@Request() req: { user: User }) {
-    console.log('3. 接口接收从 JwtStrategy 返回的内容', req.user)
-    const user = this.userService.findById(req.user.id)
-    return user
+  // 单独给指定的路由开放权限，不需要token
+  @PublicRoute()
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const user = await this.userService.findById(id)
+
+    if (user) {
+      return user
+    } else {
+      throw new BadRequestException('找不到匹配的用户')
+    }
   }
 }
